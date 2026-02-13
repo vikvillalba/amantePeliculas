144b3605939935d7aa16035464e7e9c07e491749
Victoria Villalba <vikkivillalba30@gmail.com>
2026-02-12 19:30:53 -0700

    parte 1

---
+++ amantePeliculas_VillalbaVictoria/app/src/test/java/villalba/victoria/amantepeliculas_villalbavictoria/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package villalba.victoria.amantepeliculas_villalbavictoria
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
