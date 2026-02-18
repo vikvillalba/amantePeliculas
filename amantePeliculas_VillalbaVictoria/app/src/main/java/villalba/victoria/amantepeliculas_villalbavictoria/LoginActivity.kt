608801a86d06316ead8b3c9786ee472a1334409f
vikvillalba <vikkivillalba30@gmail.com>
2026-02-17 19:18:48 -0700

    parte 3

---
+++ amantePeliculas_VillalbaVictoria/app/src/main/java/villalba/victoria/amantepeliculas_villalbavictoria/LoginActivity.kt
@@ -0,0 +1,31 @@
+package villalba.victoria.amantepeliculas_villalbavictoria
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnAcceder = findViewById<Button>(R.id.btn_acceder)
+        btnAcceder.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+}
